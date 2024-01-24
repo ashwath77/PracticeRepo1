e9aa75a3767dc089c53ff60041aa450ba6217617
ashwathgowda216@gmail.com <ashwathgowda216@gmail.com>
2024-01-24 10:27:32 +0530

    This is the for python practice

--- Python/D1_P_23-01-2024.py
+++ Python/D1_P_23-01-2024.py
@@ -1,14 +1,8 @@
 #1 Find the sum of the second largest number in a sub-list => Input = [[1, 2, 3], [2, 4, 1, 0]] Output = 2+2 = 4
 
 Input = [[1, 2, 3], [2, 4, 1, 0]]
-l=[]
-l1=[]
 sum=0
 for i in Input:
     i.sort(reverse=True)
-    l.append(i[0])
-    l1.append(i[1])
     sum+=i[1]
-print("First element in sublist: ",l)
-print("Second element in sublist: ",l1)
 print("sum of the second largest",sum)
